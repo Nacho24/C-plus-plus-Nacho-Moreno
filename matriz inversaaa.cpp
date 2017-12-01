5fb32ffa9f43269060d8c76d9f6f158dfda830a0
Nacho24 <33655548+Nacho24@users.noreply.github.com>
2017-12-01 12:23:28 +0100

    Add files via upload

---
+++ matriz inversaaa.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stdlib>
+
+
+int main(){
+  int matrix[3][3];
+  int fil,col;
+  int num;
+  for(fil=0;fil<3;fil++){
+    for(col=0;col<3;col++){
+      num=rand();
+      std::cout<<num<<" ";
+      //matrix[fil][col]= num;
+      
+    
+    
+
